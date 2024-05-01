0bbb0166d51455d95d4038ffc1179a750b72c480
Yoel <yoelsiegel@gmail.com>
2024-05-01 10:44:47 +0300

    Added annotations for functions

--- attack_cli/attack_server/attack_cli.py
+++ attack_cli/attack_server/attack_cli.py
@@ -8,14 +8,14 @@ class AttackCLI(cmd.Cmd):
     prompt = 'Prepare attack >>'
     intro = 'Welcome to Attack CLI. Type "help" for available commands.'
 
-    def do_syn_flood(self, line):
+    def do_syn_flood(self, line: str):
         create_multithreaded_attack(syn_flood_attack)
 
-    def do_url_brute_force(self, line):
+    def do_url_brute_force(self, line: str):
         create_url_wordlist()
         create_multithreaded_attack(url_buster_attack)
 
-    def do_http_flood(self, line):
+    def do_http_flood(self, line: str):
         create_multithreaded_attack(http_flood_attack)
 
     def do_quit(self, line):
